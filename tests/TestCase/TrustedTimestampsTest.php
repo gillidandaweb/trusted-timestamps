368810ee814b33172f2cfb04dd6787fd3031aea8
Fraser Murray <fraser@frasermurray.scot>
2021-03-16 19:42:37 +0000

    Look for the last line of the openssl response when validating
    Add test for testValidateWrongHashStringFails, improve testValidateWrongResponseStringFails, improve searching for messages when validating

--- tests/TestCase/TrustedTimestampsTest.php
+++ tests/TestCase/TrustedTimestampsTest.php
@@ -5,11 +5,17 @@ namespace TrustedTimestamps\Test\TestCase;
 use PHPUnit\Framework\TestCase;
 use TrustedTimestamps\TrustedTimestamps;
 
-class XsdToArrayTest extends TestCase
+class TrustedTimestampsTest extends TestCase
 {
-    const TSA_URL = 'http://zeitstempel.dfn.de';
+    const TSA_URL = 'https://freetsa.org/tsr';
 
+    private $tsaCertificate;
     private $toUnlink = array();
+
+    public function setUp(): void
+    {
+        $this->tsaCertificate = dirname(dirname(__FILE__)) . '/cacert.pem';
+    }
 
     public function testAll()
     {
@@ -26,6 +32,9 @@ class XsdToArrayTest extends TestCase
         $timestamp = TrustedTimestamps::getTimestampFromAnswer($signature['response_string']);
         $this->assertTrue(!empty($timestamp));
 
+        $valid = TrustedTimestamps::validate($sha1, $signature['response_string'], $signature['response_time'], $this->tsaCertificate);
+        $this->assertTrue($valid);
+
         $sha256 = hash('sha256', 'foo');
         /** @var \PHPUnit\Framework\string|string $requestFile (suppress IDE warning) */
         $requestFile = TrustedTimestamps::createRequestfile($sha256, 'sha256');
@@ -37,7 +46,50 @@ class XsdToArrayTest extends TestCase
         $this->assertTrue(!empty($signature['response_string']));
     }
 
-    public function tearDown()
+    public function testValidateWrongHashStringFails()
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessageMatches('/message imprint mismatch/');
+
+        $sha1 = sha1('foo');
+        $requestFile = TrustedTimestamps::createRequestfile($sha1);
+        $this->toUnlink[] = $requestFile;
+        $signature = TrustedTimestamps::signRequestfile($requestFile, self::TSA_URL);
+
+        TrustedTimestamps::validate(sha1('foo1'), $signature['response_string'], $signature['response_time'], $this->tsaCertificate);
+    }
+
+    public function testValidateWrongResponseTimeFails()
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('The responsetime of the request was changed');
+
+        $sha1 = sha1('foo');
+        $requestFile = TrustedTimestamps::createRequestfile($sha1);
+        $this->toUnlink[] = $requestFile;
+        $signature = TrustedTimestamps::signRequestfile($requestFile, self::TSA_URL);
+
+        TrustedTimestamps::validate($sha1, $signature['response_string'], 1, $this->tsaCertificate);
+    }
+
+    public function testValidateWrongResponseStringFails()
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessageMatches('/Verification: FAILED/');
+
+        $sha1 = sha1('foo');
+        $requestFile = TrustedTimestamps::createRequestfile($sha1);
+        $this->toUnlink[] = $requestFile;
+        $signature = TrustedTimestamps::signRequestfile($requestFile, self::TSA_URL);
+
+        $wrongRequestFile = TrustedTimestamps::createRequestfile(sha1('foo1'));
+        $this->toUnlink[] = $wrongRequestFile;
+        $wrongSignature = TrustedTimestamps::signRequestfile($wrongRequestFile, self::TSA_URL);
+
+        TrustedTimestamps::validate($sha1, $wrongSignature['response_string'], $signature['response_time'], $this->tsaCertificate);
+    }
+
+    public function tearDown(): void
     {
         parent::tearDown();
         foreach ($this->toUnlink as $file) {
